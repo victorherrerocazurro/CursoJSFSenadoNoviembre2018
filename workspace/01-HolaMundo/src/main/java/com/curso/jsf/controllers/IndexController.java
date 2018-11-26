63283c9b109c35000b684fba0c15be3e980c1504
Victor Herrero Cazurro <victorherrerocazurro@gmail.com>
2018-11-26 14:23:56 +0100

    Primer Dia

---
+++ workspace/01-HolaMundo/src/main/java/com/curso/jsf/controllers/IndexController.java
@@ -0,0 +1,23 @@
+package com.curso.jsf.controllers;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean //Equivale a la CDI @Named
+@RequestScoped //No es necesario ponerlo, es el por defecto
+public class IndexController {
+	
+	private Saludo saludo = new Saludo();
+
+	public Saludo getSaludo() {
+		return saludo;
+	}
+
+	public void setSaludo(Saludo saludo) {
+		this.saludo = saludo;
+	}
+
+	public void saludar(){
+		saludo.generarSaludo();
+	}
+}
